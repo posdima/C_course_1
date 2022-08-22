fe52c522e344ddb6f26b1caf446e2dec08612346
Pos Dima <zpfc@mail.ru>
2022-08-22 21:55:50 +0300

    Добавил домашнее задание 2_2. Ошибка в том, что не сравнивает площадь квадрата

---
+++ Homework_2_2/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+//DZ-2. Programma opredelenja ploscahdi figury i sravnenia ih mezhdu soboj
+
+int main()
+{
+    double radius, triangle_side, square_side;
+    double circle_square, triangle_square, square_area;
+
+    printf("Enter circle radius R = ");
+    scanf("%lf", &radius);
+    circle_square = (M_PI) * pow(radius,2);
+    printf("Circle square = %.2lf\n", circle_square);
+
+    printf("Enter equilateral triangle side a = ");
+    scanf("%lf", &triangle_side);
+    triangle_square = ((pow(triangle_side,2))*sqrt(3))/4;
+    printf("Equilateral triangle square = %.2lf\n", triangle_square);
+
+    printf("Enter square side d = ");
+    scanf("%lf", &square_side);
+    square_area = pow(square_side,2);
+    printf("Square area = %.2lf\n", square_area);
+
+    (circle_square>triangle_square&&square_area)?(printf("Circle square is the biggest\n")):(triangle_square>circle_square&&square_area)?(printf("Equilateral triangle square is the biggest\n")):(printf("Square area is the biggest\n"));
+
+
+
+
+    return 0;
+}
