4cce7787e1ca5dc0a791d0ea5127b243af36f398
Pos Dima <zpfc@mail.ru>
2022-09-01 22:54:35 +0300

    Сделал с использованием цикла while

--- Homework_3_4/main.cpp
+++ Homework_3_4/main.cpp
@@ -9,10 +9,14 @@ int main()
     scanf("%d", &decimal_number);
     int binary_number = 0;
 
-    for(int i=0; decimal_number>0; ++i)
+    int counter;
+    counter = 0;
+
+    while(decimal_number>0)
     {
-        binary_number += (decimal_number%2) * pow(10, i);
+        binary_number += (decimal_number%2) * pow(10, counter);
         decimal_number/=2;
+        ++counter;
     }
 
     printf("This number in binary system is: %d\n", binary_number);
