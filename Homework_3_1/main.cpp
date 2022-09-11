67e9b704d5ccd87ff662155f21fe90092c509547
Pos Dima <zpfc@mail.ru>
2022-09-12 01:03:15 +0300

    Pushed Homework_3_1 with number_counter.h and number_counter.cpp

--- Homework_3_1/main.cpp
+++ Homework_3_1/main.cpp
@@ -1,20 +1,15 @@
+//Дано натуральное число N. Составить программу для определения количества цифр в этом числе
+
+#include "number_counter.h"
 #include <stdio.h>
-//Дано натуральное число N. Составить программу для определения количества цифр в этом числе.
 
 int main()
 {
     printf("Enter natural number N: ");
     int N;
     scanf("%d", &N);
-    int counter = 0;
 
-    while(N%10 != 0)
-    {
-        N/=10;
-        counter++;
-    }
-
-    printf("N consists of %d numbers\n", counter);
+    numberCounter(N);
 
     return 0;
 }
