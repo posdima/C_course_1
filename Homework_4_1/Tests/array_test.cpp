1f7975d6114b6918569fff54b747a49068642766
Pos Dima <zpfc@mail.ru>
2022-09-16 00:14:01 +0300

    Test for Homework_4_1

--- Homework_4_1/Tests/array_test.cpp
+++ Homework_4_1/Tests/array_test.cpp
@@ -2,7 +2,14 @@
 
 #include "../array_swap.h"
 
+//void swap(int* M, int x)
+
 TEST(CheckSwapPositions, NumbersTest)
 {
+    int M[] = {1, 3, 8, 5, -3, 2};
 
+    swap (M, 6);
+
+    EXPECT_EQ(M[2], -3);
+    EXPECT_EQ(M[4], 8);
 }
