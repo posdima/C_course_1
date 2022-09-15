cd310732dfdd012bf23543673e33880d2ef59665
Pos Dima <zpfc@mail.ru>
2022-09-16 01:16:54 +0300

    Issue elimination

--- Homework_3_4/binary_number.cpp
+++ Homework_3_4/binary_number.cpp
@@ -6,7 +6,7 @@ int binaryNumber(int decimal_number)
     int binary_number = 0;
     int counter = 0;
 
-    while(decimal_number>0)
+    while(decimal_number)
     {
         binary_number += (decimal_number%2) * pow(10, counter);
         decimal_number/=2;
